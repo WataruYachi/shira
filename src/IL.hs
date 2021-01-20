a73cb10fc8ef8b4db7e92a3bf57231f0a941e582
Wataru Yachi <yatiwataru@gmail.com>
2021-01-21 00:39:48 +0900

    フォーマッタにコードをかけた

--- src/IL.hs
+++ src/IL.hs
@@ -1,41 +1,50 @@
-module IL (IL, Ident, Addr, VarList, Opc(..), Opr(..), Reg(..), encoder, testEncoder) where
+module IL (
+    IL,
+    Ident,
+    Addr,
+    VarList,
+    Opc (..),
+    Opr (..),
+    Reg (..),
+    encoder,
+    testEncoder,
+) where
 
-import Syntax
 import Control.Monad.State
 import qualified Data.Map.Strict as M
+import Syntax
 
 data Opc
-    = ADD  Opr Opr
-    | SUB  Opr Opr
-    | MUL  Opr Opr
-    | DIV  Opr Opr
+    = ADD Opr Opr
+    | SUB Opr Opr
+    | MUL Opr Opr
+    | DIV Opr Opr
     | PUSH Opr
-    | POP  Opr
+    | POP Opr
     | LD Opr Opr
     | ST Opr Opr
     | RT Opr
-    deriving Show
+    deriving (Show)
 
 data Opr
     = Im Int
     | Addr Addr
     | R Reg
-    deriving Show
+    deriving (Show)
 
 data Reg
     = R1
     | R2
-    deriving Show
+    deriving (Show)
 
 type Addr = Int
 type IL = [Opc]
 
 type VarList = M.Map Ident Addr
 
-data Env = Env {varNum :: Int, varList :: VarList, code :: Program} deriving Show
+data Env = Env {varNum :: Int, varList :: VarList, code :: Program} deriving (Show)
 
 type Encoder = State Env IL
-
 
 encoder' :: Program -> (IL, Env)
 encoder' p = runState encode (newEnv 0 M.empty p)
@@ -46,48 +55,49 @@ testEncoder :: Program -> IO ()
 testEncoder p = mapM_ print (evalState encode (newEnv 0 M.empty p))
 
 newEnv :: Int -> VarList -> Program -> Env
-newEnv vn vl c = Env {varNum = vn, varList = vl, code = c} 
+newEnv vn vl c = Env{varNum = vn, varList = vl, code = c}
 
 encode :: Encoder
 encode = do
     env <- get
-    if not . null $ code env then do
-        let (c:cs) = code env
-            varl = varList env
-            varn = varNum env
-        case c of
-            AS (Let i e) -> do
-                let newvarl = M.insert i varn varl
-                    il = encExpr newvarl e
-                put $ newEnv (varn+1) newvarl cs
-                next <- encode
-                return $ il ++ next
-            Return e -> do
-                let il = encExpr varl e
-                put $ newEnv varn varl cs
-                return $ il ++ [POP (R R1), RT (R R1)]
-    else do
-        return []
+    if not . null $ code env
+        then do
+            let (c : cs) = code env
+                varl = varList env
+                varn = varNum env
+            case c of
+                AS (Let i e) -> do
+                    let newvarl = M.insert i varn varl
+                        il = encExpr newvarl e
+                    put $ newEnv (varn + 1) newvarl cs
+                    next <- encode
+                    return $ il ++ next
+                Return e -> do
+                    let il = encExpr varl e
+                    put $ newEnv varn varl cs
+                    return $ il ++ [POP (R R1), RT (R R1)]
+        else do
+            return []
 
 encExpr :: M.Map Ident Addr -> Expr -> [Opc]
 encExpr varl (Expr e) = encExpr0 varl e
-encExpr varl (OpE  i) = encInfix2 varl i
+encExpr varl (OpE i) = encInfix2 varl i
 
 encInfix2 :: M.Map Ident Addr -> Infix2 -> [Opc]
 encInfix2 varl (Add e0 e) =
-    encExpr0 varl e0 ++ encExpr varl e ++
-         [ POP (R R2)
-        , POP (R R1)
-        , ADD (R R1) (R R2)
-        , PUSH (R R1)
-        ]
+    encExpr0 varl e0 ++ encExpr varl e
+        ++ [ POP (R R2)
+           , POP (R R1)
+           , ADD (R R1) (R R2)
+           , PUSH (R R1)
+           ]
 encInfix2 varl (Sub e0 e) =
-    encExpr0 varl e0 ++ encExpr varl e ++
-        [ POP (R R2)
-        , POP (R R1)
-        , SUB (R R1) (R R2)
-        , PUSH (R R1)
-        ]
+    encExpr0 varl e0 ++ encExpr varl e
+        ++ [ POP (R R2)
+           , POP (R R1)
+           , SUB (R R1) (R R2)
+           , PUSH (R R1)
+           ]
 
 encExpr0 :: M.Map Ident Addr -> Expr0 -> [Opc]
 encExpr0 varl (Expr0 e1) = encExpr1 varl e1
@@ -95,27 +105,27 @@ encExpr0 varl (OpE0 i) = encInfix3 varl i
 
 encInfix3 :: M.Map Ident Addr -> Infix3 -> [Opc]
 encInfix3 varl (Mul e1 e0) =
-    encExpr1 varl e1 ++ encExpr0 varl e0 ++
-        [ POP (R R2)
-        , POP (R R1)
-        , MUL (R R1) (R R2)
-        , PUSH (R R1)
-        ]
+    encExpr1 varl e1 ++ encExpr0 varl e0
+        ++ [ POP (R R2)
+           , POP (R R1)
+           , MUL (R R1) (R R2)
+           , PUSH (R R1)
+           ]
 encInfix3 varl (Div e1 e0) =
-    encExpr1 varl e1 ++ encExpr0 varl e0 ++
-        [ POP (R R2)
-        , POP (R R1)
-        , DIV (R R1) (R R2)
-        , PUSH (R R1)
-        ]
+    encExpr1 varl e1 ++ encExpr0 varl e0
+        ++ [ POP (R R2)
+           , POP (R R1)
+           , DIV (R R1) (R R2)
+           , PUSH (R R1)
+           ]
 
 encExpr1 :: M.Map Ident Addr -> Expr1 -> [Opc]
 encExpr1 varl (Expr1 e) = encExpr varl e
 encExpr1 varl (Int i) = [PUSH (Im i)]
-encExpr1 varl (Var ident) = 
-    let a = M.lookup ident varl in
-        case a of
-            Just a -> 
+encExpr1 varl (Var ident) =
+    let a = M.lookup ident varl
+     in case a of
+            Just a ->
                 [ LD (R R1) (Addr a)
                 , PUSH (R R1)
                 ]
--- src/VM.hs
+++ src/VM.hs
@@ -1,25 +1,25 @@
 module VM where
 
-import IL
-import Parser
 import Control.Monad.State
 import qualified Data.Map.Strict as M
+import IL
+import Parser
 
 type Stack = [Int]
 
-data VReg
-    = VReg
+data VReg = VReg
     { r1 :: Int
     , r2 :: Int
-    } deriving Show
+    }
+    deriving (Show)
 
-data VEnv
-    = VEnv
+data VEnv = VEnv
     { stack :: Stack
     , program :: IL
     , reg :: VReg
     , varList :: VarList
-    } deriving Show
+    }
+    deriving (Show)
 
 type VM = State VEnv Int
 
@@ -27,61 +27,60 @@ vm :: VM
 vm = do
     e <- get
     let s = stack e
-        p:ps = program e
+        p : ps = program e
         rs = reg e
-        vs = varList e 
+        vs = varList e
     case p of
         (PUSH o) ->
             case o of
-                Im i -> next $ newVM (i:s) ps rs vs
-                R R1 -> next $ newVM (r1 rs:s) ps rs vs
-                R R2 -> next $ newVM (r2 rs:s) ps rs vs
+                Im i -> next $ newVM (i : s) ps rs vs
+                R R1 -> next $ newVM (r1 rs : s) ps rs vs
+                R R2 -> next $ newVM (r2 rs : s) ps rs vs
         (POP o) ->
-            let (s':ss) = s in
-                case o of
+            let (s' : ss) = s
+             in case o of
                     R r -> next $ newVM ss ps (movReg rs r s') vs
         ADD (R r1) (R r2) ->
             let l = referVReg rs r1
                 r = referVReg rs r2
-                new  = newVM s ps (movReg rs r1 (l+r)) vs
-            in  next new
+                new = newVM s ps (movReg rs r1 (l + r)) vs
+             in next new
         SUB (R r1) (R r2) ->
             let l = referVReg rs r1
                 r = referVReg rs r2
-                new  = newVM s ps (movReg rs r1 (l-r)) vs
-            in  next new
+                new = newVM s ps (movReg rs r1 (l - r)) vs
+             in next new
         MUL (R r1) (R r2) ->
             let l = referVReg rs r1
                 r = referVReg rs r2
-                new  = newVM s ps (movReg rs r1 (l*r)) vs
-            in  next new
+                new = newVM s ps (movReg rs r1 (l * r)) vs
+             in next new
         DIV (R r1) (R r2) ->
             let l = referVReg rs r1
                 r = referVReg rs r2
-                new  = newVM s ps (movReg rs r1 (l+r)) vs
-            in  next new
-        LD (R r) (Addr a) -> let a' = (length s - a) - 1 in
-            next $ newVM s ps (movReg rs r (s !! a')) vs
+                new = newVM s ps (movReg rs r1 (l + r)) vs
+             in next new
+        LD (R r) (Addr a) ->
+            let a' = (length s - a) - 1
+             in next $ newVM s ps (movReg rs r (s !! a')) vs
         ST a b -> undefined
         RT (R r) -> return $ referVReg rs r
-    where
-        next e = put e >> vm 
-            
-
-                        
+  where
+    next e = put e >> vm
 
 newVM :: Stack -> IL -> VReg -> VarList -> VEnv
-newVM s l r v = VEnv {stack = s, program = l, reg = r, varList = v} 
+newVM s l r v = VEnv{stack = s, program = l, reg = r, varList = v}
 
 movReg :: VReg -> IL.Reg -> Int -> VReg
-movReg regs IL.R1 x = VReg {r1 = x, r2 = r2 regs}
-movReg regs IL.R2 x = VReg {r1 = r1 regs, r2 = x} 
+movReg regs IL.R1 x = VReg{r1 = x, r2 = r2 regs}
+movReg regs IL.R2 x = VReg{r1 = r1 regs, r2 = x}
 
-referVReg :: VReg -> IL.Reg -> Int 
+referVReg :: VReg -> IL.Reg -> Int
 referVReg reg IL.R1 = r1 reg
 referVReg reg IL.R2 = r2 reg
 
-runVM s = runState vm $ newVM [] (encoder p) VReg {r1 = 0, r2 = 0} M.empty
-    where p = case makeAST s of
-                Just (c,s) -> c
-                Nothing -> []
+runVM s = runState vm $ newVM [] (encoder p) VReg{r1 = 0, r2 = 0} M.empty
+  where
+    p = case makeAST s of
+        Just (c, s) -> c
+        Nothing -> []
