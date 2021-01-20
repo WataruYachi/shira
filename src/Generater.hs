4dbe112abda4acc940a119d17bc1ad29c2df3521
Wataru Yachi <yatiwataru@gmail.com>
2021-01-21 00:53:46 +0900

    gen書きかけ（LD,STを追加したい）

--- src/Generater.hs
+++ src/Generater.hs
@@ -1,7 +1,6 @@
 module Generater where
 
-import Syntax
-
+import IL
 import Parser
 
 import System.Environment
@@ -12,6 +11,7 @@ import qualified Data.Text as T
 
 import Control.Monad.State
 
+{-
 type OffsetFromSP = Int
 data Compiler
     = Compiler
@@ -24,6 +24,29 @@ data Compiler
 -- 構文木を読み進めながら、各ステートメントに対応したアセンブラを生成する。
 complier :: State Compiler Compiler
 complier = undefined 
+-}
+
+gen :: IL -> [String]
+gen [] = []
+gen (c:cs) =
+    case c of
+        PUSH o ->
+            let opr = case o of
+                        Im i -> show i
+                        R R1 -> "rax"
+                        R R2 -> "rcx"
+                in ("push " ++ opr) : gen cs
+        POP o ->
+            let opr = case o of
+                        R R1 -> "rax"
+                        R R2 -> "rac"
+                in ("pop " ++ opr) : gen cs
+        ADD _ _ -> "add rax, rcx" : gen cs
+        SUB _ _ -> "sub rax, rcx" : gen cs
+        MUL _ _ -> "imul rax, rcx" : gen cs
+        DIV _ _ -> "cqo" : "idiv rcx" : gen c
+        LD r a -> case r of
+                    R R1 -> ""
 
 generateAsm = undefined
 
